8b032497e70a80a0e3ab10b006c17cb5864d0451
lsylion <sylliconn.c@gmail.com>
2025-07-17 06:28:47 +0000

    change from task2

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ public class Main {
     public void main(String[] args) {
         // This is a sample Java program
         System.out.println("This is a sample Java program.");
+        System.out.println("This is from task2 branch.");
 
-        System.out.println("This is from task2 branch.")
-        
     }
    
 }
